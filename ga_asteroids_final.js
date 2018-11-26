c1705fe9d36391414305a1cceb5f2ffb092115c3
wendellalves <wendell.a.silva2@gmail.com>
2018-11-26 15:26:54 -0300

    GA quage final

--- ga_asteroids_final.js
+++ ga_asteroids_final.js
@@ -9,26 +9,24 @@ class Cromo {
         this.apitidao = pontuacao;
     }
 
-    apitidao(){
-        return this.apitidao;
-    }
-
     mutate() {
-        indice1 = Math.floor((Math.random() * 4));
-        indice2 = Math.floor((Math.random() * 4));
-        while(indice1 == indice2){ 
-            indice2 = Math.floor((Math.random() * 4));
+        let indice1 = Math.floor((Math.random() * this.weigths.length));
+        let indice2 = Math.floor((Math.random() * this.weigths.length));
+
+        while(indice1 === indice2) { 
+            indice2 = Math.floor((Math.random() * this.weigths.length));
         }
-        aux = this.weigths[indice1];
-        this.weigths[indice1] = this.weigths[indice2];
-        this.weigths[indice2] = aux;
+
+        let aux = this.weigths[indice1]; 
+        //this.weigths[indice1] = this.weigths[indice2];
+        //this.weigths[indice2] = aux;
     }
 
     cruzamento(cromo) {
-        let pivot = Math.round(this.weigths.length / 2) - 1;
+        let pivot = Math.round(this.weigths.length / 2);
 
-        let child1 = this.weigths.substr(0, pivot) + cromo.weigths.substr(pivot);
-        let child2 = cromo.weigths.substr(0, pivot) + this.weigths.substr(pivot);
+        let child1 = this.weigths.slice(0, pivot) + cromo.weigths.slice(pivot, this.weigths.length);
+        let child2 = cromo.weigths.slice(0, pivot)  + this.weigths.slice(pivot, this.weigths.length);
 
         return [new Cromo(child1, 0), new Cromo(child2, 0)];
     }
@@ -45,51 +43,70 @@ class Population {
         this.tx_cruzamento = 0.1;
     }
 
-    seleciona_roleta(individuos){
-        for (let index = 0; index < this.individuos.length; index++) {
-            this.somatotal = individuos[i].apitidao();
-        }
-        r = Math.floor((Math.random() * this.somatotal)); // Escolhe um valor aleatorio entre 0 e soma
-        soma2 = 0;
-        for (let index = 0; index < this.individuos.length; index++) {
-            soma2 += individuos[i][0];
-            if (soma2 > r) {
-                return index;
+    seleciona_roleta(){
+        let somatotal = 0;
+        this.individuos.forEach(individio => {
+            somatotal += individio.apitidao;
+        });
+        
+        let r = Math.floor((Math.random() * somatotal)); // Escolhe um valor aleatorio entre 0 e soma
+        let soma2 = 0;
+
+        this.individuos.forEach(individio => {
+            soma2 += individio.apitidao;
+
+            if(soma2 > r) {
+                return individio;
             }
-        }
-        return this.size - 1;
+
+        });
+
+        return this.individuos[this.individuos.length - 1];
     }
 
     sort() {
-        this.individuos.sort( (a, b) => (a.apitidao - b.apitidao) );
+        this.individuos.sort( (a, b) => (b.apitidao - a.apitidao) );
     }
 
     generation() {
-        elite = [];
-        filhos = [];
+        let elite = [];
+        let filhos = [];
         
         this.sort();
 
-        for (let index = 0; index < (this.individuos.length * (1 - tx_cruzamento)); index++) {//removendo elite
-            elite += this.individuos[this.individuos.length];
-            this.individuos.pop();
+        this.individuos = this.individuos.slice((0, this.individuos.length / 2) + 1);
+
+        for (let index = 0; index < Math.ceil(this.individuos.length * this.tx_cruzamento); index++) {//removendo elite
+            elite.push(this.individuos[index]);
+            this.individuos.slice(index, 1);
+        }
+        console.log("Elite", elite)
+
+        //selecionando por roleta e fazendo cruzamento
+        for (let index = 0; index < this.individuos.length ; index++) {
+            let selecionado1 = this.seleciona_roleta(this.individuos);
+            let selecionado2 = this.seleciona_roleta(this.individuos); 
+
+            let novos_filhos = selecionado1.cruzamento(selecionado2);
+            
+            novos_filhos.forEach(filho => {
+                filhos.push(filho);
+            });
         }
 
-        for (let index = 0; index < this.individuos.length; index++) {//selecionando por roleta e fazendo cruzamento
-            selecionado1 = this.seleciona_roleta(this.individuos);
-            selecionado2 = this.seleciona_roleta(this.individuos); 
+        // Mutacao nos filhos
+        console.log("Filhos", filhos)
+        filhos.forEach(filho => {
+            filho.mutate();
+        });
 
-            filhos += this.individuos[selecionado1].cruzamento(this.individuos[selecionado2]);
-        }
+        elite.forEach(joao => {
+            filhos.push(joao);
+        });
 
-        for (let index = 0; index < filhos.length; index++) {//mutacao nos filhos
-            filhos[index] = filhos[index].mutate();  
-        }
+        this.individuos = filhos;
 
-        filhos += elite;
-        this.individuos += filhos;
-
-        this.individuos.sort( (a, b) => ( 0.5 - Math.random()) );
+        this.individuos.sort( () => ( 0.5 - Math.random()) );
 
         return this.individuos;
 
@@ -97,5 +114,15 @@ class Population {
 
 }
 
-let population = new Population("-123456789", 30);
-pesos_mlp = population.generation();
+popu = new Population([
+    new Cromo([1, 2, 3, 4], 100), 
+    new Cromo([6, 7, 8, 9], 98), 
+    new Cromo([10, 11, 12, 13], 38),
+    new Cromo([1, 21, 3, 4], 70), 
+    new Cromo([6, 17, 8, 9], 98), 
+    new Cromo([10, 31, 12, 13], 78),
+    new Cromo([15, 27, 23, 4], 50), 
+    new Cromo([6, 7, 8, 9], 38), 
+    new Cromo([10, 19, 21, 13], 138),
+    new Cromo([13, 27, 3, 4], 115)
+])
